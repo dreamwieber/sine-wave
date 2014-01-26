7fa63bdb8daf9f551047d2fae699db2ebb6b46b8
dreamwieber <gregorywieber@gmail.com>
2014-01-26 14:04:30 -0800

    Initial Commit

---
+++ SineWaveTests/SineWaveTests.m
@@ -0,0 +1,34 @@
+//
+//  SineWaveTests.m
+//  SineWaveTests
+//
+//  Created by Gregory Wieber on 1/26/14.
+//  Copyright (c) 2014 Apposite. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SineWaveTests : XCTestCase
+
+@end
+
+@implementation SineWaveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
